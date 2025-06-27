b966e56cbfa1d05ac70a2ab34bc0cd3f706c151d
Jakub Szubarga <jakub.szubarga@checkatrade.com>
2025-06-27 14:12:45 +0200

    Test

--- OneToManyFlows.AuthorizationApi/Controllers/WeatherForecastController.cs
+++ OneToManyFlows.AuthorizationApi/Controllers/WeatherForecastController.cs
@@ -4,7 +4,7 @@ using Microsoft.Identity.Web.Resource;
 
 namespace OneToManyFlows.AuthorizationApi.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [ApiController]
     [Route("[controller]")]
     [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
--- OneToManyFlows.AuthorizationApi/Program.cs
+++ OneToManyFlows.AuthorizationApi/Program.cs
@@ -5,8 +5,8 @@ using Scalar.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -20,7 +20,7 @@ app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
